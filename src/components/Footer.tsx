a02ca7683e0566ad2a998b0c902984987c7cb513
kritical0613 <kritical0613@outlook.com>
2022-04-05 13:46:07 -0500

    feat: Founder & Footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom"
+import OPENSEA_LIGHT from "../assets/svg/opensea-light.svg"
+import LOOKSRARE_LIGHT from "../assets/svg/looksrare-light.svg"
+import TWITTER_LIGHT from "../assets/svg/twitter-light.svg"
+import MEDIUM_LIGHT from "../assets/svg/medium-light.svg"
+import INSTAGRAM_LIGHT from "../assets/svg/instagram-light.svg"
+import kaijuLinks from "../configs/kaijuLinks.json"
+
 const Footer: React.FC = () => {
   return (
-    <></>
+    <div className="bg-dark-blue pt-16 pb-20 mt-20">
+      <div className="flex items-center justify-center gap-8">
+        <a href={kaijuLinks.opensea} target="_blank" rel="noreferrer"><img src={OPENSEA_LIGHT} alt="OpenSea" /></a>
+        <a href={kaijuLinks.looksrare} target="_blank" rel="noreferrer"><img src={LOOKSRARE_LIGHT} alt="Looksrare" /></a>
+        <a href={kaijuLinks.twitter} target="_blank" rel="noreferrer"><img src={TWITTER_LIGHT} alt="Twitter" /></a>
+        <a href={kaijuLinks.medium} target="_blank" rel="noreferrer"><img src={MEDIUM_LIGHT} alt="Medium" /></a>
+        <a href={kaijuLinks.instagram} target="_blank" rel="noreferrer"><img src={INSTAGRAM_LIGHT} alt="Instagram" /></a>
+      </div>
+    </div>
   )
 }
 
--- src/components/Founder.tsx
+++ src/components/Founder.tsx
@@ -1,6 +1,17 @@
+import KAIJU_WAVING from "../assets/png/Kaiju Waving.png"
+
 const Founder: React.FC = () => {
   return (
-    <></>
+    <div className="grid justify-items-center items-center gap-10 bg-light-oliver mt-20">
+      <div className="font-extrabold text-dark-blue uppercase text-[3rem] mt-24">About the founder</div>
+      <div className="grid w-3/5 text-center text-[1.5rem] gap-10">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div>
+      <div className="flex space-x-20 my-10">
+        <img src={KAIJU_WAVING} alt="Kaiju Waving" width={300} height={262} />
+      </div>
+    </div>
   )
 }
 
