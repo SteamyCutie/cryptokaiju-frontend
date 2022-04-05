b9f091902c5e55eacde4b111fa94f3038e055c51
kritical0613 <kritical0613@outlook.com>
2022-04-05 02:06:03 -0500

    feat: page assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'light-blue': '#1282a2',
+      'dark-blue': '#1f2760',
+      'light-oliver': '#dfd4d0',
+      'light-sky': '#f0f8f5',
+      'dark-grey': '#dddddd'
+    },
   },
   plugins: [],
 }
