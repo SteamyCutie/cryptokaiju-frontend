3ce6624440ac4b5dc479ae3b4e9169dc63aca4b8
kritical0613 <kritical0613@outlook.com>
2022-04-05 20:07:54 -0500

    fix: Header spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,12 +10,12 @@ const Header: React.FC = () => {
   return (
     <div className="bg-light-blue justify-between w-full items-center flex px-10 py-8">
       <Link to=""><img src={DINO_LOGO} alt="Dino Logo" width={80} height={80} /></Link>
-      <div className="text-black text-[1.5rem] font-bold uppercase flex gap-10">
+      <div className="text-black text-[1.5rem] font-bold uppercase flex gap-10 tracking-widest">
         <div className="cursor-pointer">About</div>
         <a href={kaijuLink.shop} target="_blank" rel="noreferrer">Shop</a>
         <div className="cursor-pointer">FAQ</div>
       </div>
-      <div className="text-black flex items-center gap-2">
+      <div className="text-black flex items-center gap-4">
         <a href={kaijuLink.opensea} target="_blank" rel="noreferrer"><img src={OPENSEA_ICON} alt="Dino Logo" width={40} height={40} /></a>
         <a href={kaijuLink.looksrare} target="_blank" rel="noreferrer"><img src={LOOKSRARE_ICON} alt="Dino Logo" width={40} height={40} /></a>
         <a href={kaijuLink.twitter} target="_blank" rel="noreferrer"><img src={TWITTER_ICON} alt="Dino Logo" width={40} height={40} /></a>
