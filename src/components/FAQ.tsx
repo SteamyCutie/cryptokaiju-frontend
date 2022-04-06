add76597d3c5371978823fd74e04e8e4ec946b89
kritical0613 <kritical0613@outlook.com>
2022-04-06 02:04:31 -0500

    fix: FAQ style

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -4,7 +4,7 @@ import FAQItem from "./utils/FAQItem"
 const FAQ: React.FC = () => {
   return (
     <div id="faq" className="grid justify-items-center items-center gap-10 bg-light-sky mt-20 py-10">
-      <div className="font-extrabold text-dark-blue uppercase text-[2rem] md:text-[2.5rem] lg:text-[3rem] mt-10">FAQs</div>
+      <div className="font-extrabold text-dark-blue text-[2rem] md:text-[2.5rem] lg:text-[3rem] mt-10">FAQs</div>
       <div className="grid w-4/5 lg:w-3/5 text-center text-[1rem] md:text-[1.25rem] lg:text-[1.5rem] gap-4">
         {
           kaijuFAQs.map((item, idx) =>
