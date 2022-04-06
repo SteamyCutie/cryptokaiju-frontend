7d1dd52c560fbfa948ead437dbbcd35e1dbb8aa7
kritical0613 <kritical0613@outlook.com>
2022-04-05 20:14:11 -0500

    feat: ScrollToTop button and thin scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router } from "react-router-dom"
 
 import './App.css'
@@ -10,8 +10,30 @@ import Founder from './components/Founder'
 import Header from './components/Header'
 import Mint from './components/Mint'
 import Press from './components/Press'
+import { ScrollUpButton } from './components/utils/ScrollUpButton'
 
 const App: React.FC = () => {
+
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      // setOffset(window.pageYOffset > 500 ? 500 : window.pageYOffset);
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <div className="App">
       <Router>
@@ -23,9 +45,10 @@ const App: React.FC = () => {
         <Founder />
         <Press />
         <Footer />
+        <ScrollUpButton handler={scrollToTop} showButton={showButton} />
       </Router>
     </div>
   );
 }
 
-export default App;
+export default App
