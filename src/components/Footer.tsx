ec3dfd87a911c161b9b9cb7be6582825db5fd921
kritical0613 <kritical0613@outlook.com>
2022-04-06 02:02:10 -0500

    feat: Footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,12 +8,12 @@ import kaijuLinks from "../configs/kaijuLinks.json"
 const Footer: React.FC = () => {
   return (
     <div className="bg-dark-blue pt-16 pb-20 mt-20">
-      <div className="flex items-center justify-center gap-8">
-        <a href={kaijuLinks.opensea} target="_blank" rel="noreferrer"><img src={OPENSEA_LIGHT} alt="OpenSea" /></a>
-        <a href={kaijuLinks.looksrare} target="_blank" rel="noreferrer"><img src={LOOKSRARE_LIGHT} alt="Looksrare" /></a>
-        <a href={kaijuLinks.twitter} target="_blank" rel="noreferrer"><img src={TWITTER_LIGHT} alt="Twitter" /></a>
-        <a href={kaijuLinks.medium} target="_blank" rel="noreferrer"><img src={MEDIUM_LIGHT} alt="Medium" /></a>
-        <a href={kaijuLinks.instagram} target="_blank" rel="noreferrer"><img src={INSTAGRAM_LIGHT} alt="Instagram" /></a>
+      <div className="flex items-center justify-center gap-4 md:gap-6 lg:gap-8">
+        <a className="hover:scale-[1.02] active:scale-[0.98] transition-all duration-150 ease-out w-6 md:w-8 lg:w-10" href={kaijuLinks.opensea} target="_blank" rel="noreferrer"><img src={OPENSEA_LIGHT} alt="OpenSea" /></a>
+        <a className="hover:scale-[1.02] active:scale-[0.98] transition-all duration-150 ease-out w-6 md:w-8 lg:w-10" href={kaijuLinks.looksrare} target="_blank" rel="noreferrer"><img src={LOOKSRARE_LIGHT} alt="Looksrare" /></a>
+        <a className="hover:scale-[1.02] active:scale-[0.98] transition-all duration-150 ease-out w-6 md:w-8 lg:w-10" href={kaijuLinks.twitter} target="_blank" rel="noreferrer"><img src={TWITTER_LIGHT} alt="Twitter" /></a>
+        <a className="hover:scale-[1.02] active:scale-[0.98] transition-all duration-150 ease-out w-6 md:w-8 lg:w-10" href={kaijuLinks.medium} target="_blank" rel="noreferrer"><img src={MEDIUM_LIGHT} alt="Medium" /></a>
+        <a className="hover:scale-[1.02] active:scale-[0.98] transition-all duration-150 ease-out w-6 md:w-8 lg:w-10" href={kaijuLinks.instagram} target="_blank" rel="noreferrer"><img src={INSTAGRAM_LIGHT} alt="Instagram" /></a>
       </div>
     </div>
   )
