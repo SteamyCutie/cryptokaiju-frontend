0c81438972173ef74e6cde1dcd4c2114e5b7de87
kritical0613 <kritical0613@outlook.com>
2022-04-05 20:33:43 -0500

    fix: Carousel style

--- src/components/Press.tsx
+++ src/components/Press.tsx
@@ -1,5 +1,6 @@
 import Carousel from "nuka-carousel"
 import kaijuPress from "../configs/kaijuPress.json"
+import ARROW from "../assets/svg/arrow.svg"
 
 const Press: React.FC = () => {
   return (
@@ -8,23 +9,22 @@ const Press: React.FC = () => {
       <div className="flex space-x-20 mb-10 bg-dark-white w-full items-center justify-center">
         <Carousel
           renderCenterLeftControls={({ previousSlide }) => (
-            <button onClick={previousSlide} className="btn">
-              <svg version="1.1" className="fill-white" style={{ transform: `rotate(90deg)` }} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 30.021 30.021" xmlSpace="preserve">
-                <g><path d="M28.611,13.385l-11.011,9.352c-0.745,0.633-1.667,0.949-2.589,0.949c-0.921,0-1.842-0.316-2.589-0.949L1.411,13.385 c-1.684-1.43-1.89-3.954-0.46-5.638c1.431-1.684,3.955-1.89,5.639-0.459l8.421,7.151l8.42-7.151 c1.686-1.43,4.209-1.224,5.639,0.459C30.5,9.431,30.294,11.955,28.611,13.385z" /></g>
-              </svg>
+            <button onClick={previousSlide} className="mx-10">
+              <img src={ARROW} width={24} height={24} alt="Arrow" style={{ transform: "rotate(90deg)" }} />
             </button>
           )}
           renderCenterRightControls={({ nextSlide }) => (
-            <button onClick={nextSlide} className="btn">
-              <svg version="1.1" className="fill-white" style={{ transform: `rotate(-90deg)` }} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 30.021 30.021" xmlSpace="preserve">
-                <g><path d="M28.611,13.385l-11.011,9.352c-0.745,0.633-1.667,0.949-2.589,0.949c-0.921,0-1.842-0.316-2.589-0.949L1.411,13.385 c-1.684-1.43-1.89-3.954-0.46-5.638c1.431-1.684,3.955-1.89,5.639-0.459l8.421,7.151l8.42-7.151 c1.686-1.43,4.209-1.224,5.639,0.459C30.5,9.431,30.294,11.955,28.611,13.385z" /></g>
-              </svg>
+            <button onClick={nextSlide} className="mx-10">
+              <img src={ARROW} width={24} height={24} alt="Arrow" style={{ transform: "rotate(-90deg)" }} />
             </button>
           )}
           style={{ height: 360, padding: 0 }}
           className="text-center w-[100vw]"
           autoplay={true}
+          cellSpacing={5}
           easing={"easeQuadInOut"}
+          wrapAround={true}
+          zoomScale={0.1}
         >
           {kaijuPress.map((step, idx) =>
             <div key={idx} className="grid items-center justify-items-center text-center w-full h-[360px] mx-10">
